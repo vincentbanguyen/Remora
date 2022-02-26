d2324c91d34cf9f0430cda7c96dc7310ac8afdd9
Vincent Nguyen <vincentbanguyen@gmail.com>
2022-02-25 18:58:15 -0600

    Initial Commit

---
+++ Remora/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Remora
+//
+//  Created by Vincent Nguyen on 2/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Remora/RemoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemoraApp.swift
+//  Remora
+//
+//  Created by Vincent Nguyen on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct RemoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
