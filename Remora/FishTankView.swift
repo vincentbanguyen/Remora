827a66f8fbc459f7daf2434a40706c4e7d6f6f72
Randy T <randythai360@gmail.com>
2022-02-25 22:57:17 -0600

    FishMove

--- Remora/FishTankView.swift
+++ Remora/FishTankView.swift
@@ -3,7 +3,7 @@ import SceneKit
 
 struct FishTankView : UIViewRepresentable {
     let scene = SCNScene(named: "MainScene.scn")!
-
+    
     func makeUIView(context: Context) -> SCNView {
         
         // can do camera progarmmaticaly or in mainScene
@@ -16,11 +16,24 @@ struct FishTankView : UIViewRepresentable {
         let scnView = SCNView()
         return scnView
     }
-
+    
+    func setupNodes() -> SCNNode? {
+        let fishNode = scene.rootNode.childNode(withName: "fish", recursively: true)!
+        return fishNode
+    }
+    
+    func moveFish(fishNode: SCNNode){
+        fishNode.runAction(SCNAction.move(to: SCNVector3Make(5, 0, 5), duration: 0.25))
+    }
+    
     func updateUIView(_ scnView: SCNView, context: Context) {
         scnView.scene = scene
         scnView.allowsCameraControl = true
         scnView.backgroundColor = UIColor.black
+        
+        let fishNode = setupNodes()!
+        moveFish(fishNode: fishNode)
+        
     }
 }
 
