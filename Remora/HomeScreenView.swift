639fecbfd77293f1e95a08f464b577190f2066cb
Dhruv Chowdhary <dhruvchowdhary@berkeley.edu>
2022-02-26 13:00:15 -0800

    e

--- Remora/HomeScreenView.swift
+++ Remora/HomeScreenView.swift
@@ -1,10 +1,20 @@
 import SwiftUI
 
+
 struct HomeScreenView: View {
     @State private var phrase = "Good job"
     let name = UserDefaults.standard.string(forKey: "name") ?? "Bob"
-    let waterDrank = 2
+    @State private var waterDrank = 2
+    let timeSinceDrank = Date().timeIntervalSinceReferenceDate - UserDefaults.standard.double(forKey: "timeSinceDrank") ?? Date().timeIntervalSinceReferenceDate
+    
+    func decreaseWater() {
+        if(timeSinceDrank > 3600) {
+            waterDrank = waterDrank - Int(round(timeSinceDrank/3600))
+        }
+    }
+    
     var body: some View {
+        
         
         ZStack {
             
@@ -12,7 +22,6 @@ struct HomeScreenView: View {
                 .ignoresSafeArea()
             
             // Top Message
-            
             VStack(alignment: .leading){
                 VStack(alignment: .leading) {
                     Text("\(phrase) \(name)!")
@@ -42,7 +51,7 @@ struct HomeScreenView: View {
             // Tank View
             FishTankView()
                 .frame(width: 400, height: 400)
-        }
+        }.onAppear{decreaseWater()}
         .transition(.backslide)
     }
 }
